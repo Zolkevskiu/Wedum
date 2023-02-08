5b3e971913084a1c532b72ca047a1930144ac17b
Denis <zolkevskiu@gmail.com>
2023-02-08 12:13:40 +0200

    fix modal button

--- assets/js/diagrams.js
+++ assets/js/diagrams.js
@@ -5,8 +5,7 @@ window.addEventListener('load', function (e) {
     let data = [];
     let inters = [];
     let mostCountSum = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    // let dataSum = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    let dataSum = [1, 1, 1, 1, 1, 1, 1, 1, 1];
+    let dataSum = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     let sumKrasota = [11, 0, 11, 0, 11, 0, 11, 0];
     let mostKrasotaSum = [0, 0, 0, 0, 0, 0, 0, 0];
     let sumWorks = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
